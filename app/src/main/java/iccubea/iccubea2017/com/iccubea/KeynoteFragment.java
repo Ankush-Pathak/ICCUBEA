06b356bffc9c1f568e44276f2bb305cacb1aad32
Ankush-Pathak <ankush.pathak151196@gmail.com>
2017-07-30 13:18:30 +0530

    Keynote fragment done - excuse the colors

--- app/src/main/java/iccubea/iccubea2017/com/iccubea/KeynoteFragment.java
+++ app/src/main/java/iccubea/iccubea2017/com/iccubea/KeynoteFragment.java
@@ -8,85 +8,48 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardExpand;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.view.CardViewNative;
+import com.ramotion.foldingcell.FoldingCell;
 
 
 public class KeynoteFragment extends Fragment {
 
-
+    FoldingCell fcInauguration, fcKeynote1, fcKeynote2;
     public KeynoteFragment() {
         // Required empty public constructor
     }
-
-
-    Card card;
-    CardHeader cardHeader;
-    CardViewNative cardViewNative;
-    CardExpandInsideSquare innerSquare;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_keynote, container, false);
 
-
-
-        View view =inflater.inflate(R.layout.fragment_keynote, container, false);
-        card = new Card(getContext());
-        cardHeader = new CardHeader(getContext());
-        card.addCardHeader(cardHeader);
-
-
-
-
-        cardHeader.setTitle("Keynote Speaker 1");
-        cardHeader.setButtonExpandVisible(true);
-        CardExpand cardExpand = new CardExpand(getContext());
-        cardExpand.setTitle("Details");
-        card.addCardExpand(cardExpand);
-
-        cardViewNative = (CardViewNative) view.findViewById(R.id.cardKeynoteSpeaker1);
-        cardViewNative.setCard(card);
-
-
-
-        innerSquare = new CardExpandInsideSquare(getActivity(),1,"17/7/17","Dr. Sonali Patil","11:00 am", "Seminar Hall");
-
-
-
-        cardHeader.setTitle("Keynote Speaker 2 \n Dr. Jayant Umale");
-        cardHeader.setButtonExpandVisible(true);
-        CardExpand cardExpand2 = new CardExpand(getContext());
-        cardExpand2.setTitle("Details");
-        card.addCardExpand(cardExpand2);
-
-        cardViewNative = (CardViewNative) view.findViewById(R.id.cardKeynoteSpeaker2);
-        cardViewNative.setCard(card);
-
-        innerSquare = new CardExpandInsideSquare(getActivity(),2,"17/7/17","Dr.Jayant Umale","1:00 pm", "Room no 201");
-
-
-
-        cardHeader.setTitle("Keynote Speaker 3 \n Dr. K. Rajeswari");
-        cardHeader.setButtonExpandVisible(true);
-        CardExpand cardExpand3 = new CardExpand(getContext());
-        cardExpand3.setTitle("Details");
-        card.addCardExpand(cardExpand3);
-
-
-        cardViewNative = (CardViewNative) view.findViewById(R.id.cardKeynoteSpeaker3);
-        cardViewNative.setCard(card);
-
-        innerSquare = new CardExpandInsideSquare(getActivity(),3,"18/7/17","Dr. K. Rajeswari","10:00 am","LRDC");
-
-
-
-
-        return inflater.inflate(R.layout.fragment_keynote, container, false);
+        fcInauguration = (FoldingCell) view.findViewById(R.id.folding_cellInauguration);
+        // attach click listener to folding cell
+        fcInauguration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fcInauguration.toggle(false);
+            }
+        });
+        fcKeynote1 = (FoldingCell) view.findViewById(R.id.folding_cellKeynote1);
+        // attach click listener to folding cell
+        fcKeynote1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fcKeynote1.toggle(false);
+            }
+        });
+        fcKeynote2 = (FoldingCell) view.findViewById(R.id.folding_cellKeynote2);
+        // attach click listener to folding cell
+        fcKeynote2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fcKeynote2.toggle(false);
+            }
+        });
+        return view;
     }
 
 
